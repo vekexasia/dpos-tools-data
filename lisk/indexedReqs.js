c85428c20ed319f68c1b734515f838986dcb3cdf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-11 08:18:39 +0000

    Deploy vekexasia/dpos-tools-data to github.com/vekexasia/dpos-tools-data.git:artifacts

---
+++ lisk/indexedReqs.js
@@ -0,0 +1,20 @@
+__lisk_indexedReqs({
+  "ascend": [
+    {
+      "type": "groupvote",
+      "value": "ascend"
+    }
+  ],
+  "elite": [
+    {
+      "type": "groupvote",
+      "value": "elite"
+    }
+  ],
+  "gdt": [
+    {
+      "type": "groupvote",
+      "value": "gdt"
+    }
+  ]
+});
